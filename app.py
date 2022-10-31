a40f79025b2173c92ab28a9e99759c575e0bc82f
Gwanho KIM <bigquan977@gmail.com>
2022-11-01 03:03:47 +0900

    아이콘 삭제 및 가독성 추가

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.mbry8je.mongodb.net/?retryWrites=true&w=majority')
+db = client.johan
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/gwanho')
+def gwanho():
+    return render_template('5.html')
+
+@app.route("/gwanho", methods=["POST"])
+def gwanho_comment_post():
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+    doc = {
+        'name': name_receive,
+        'comment': comment_receive,
+    }
+    db.gwanho.insert_one(doc)
+    return jsonify({'msg': '댓글 작성 완료!'})
+
+@app.route("/gwanho-api", methods=["GET"])
+def gwanho_get():
+    find = list(db.gwanho.find({}, {'_id': False}))
+    return jsonify({'gwanho': find})
+
+@app.route("/gwanho-cheer", methods=["POST"])
+def gwanho_cheer():
+    find = list(db.gwanho.find({}, {'_id': False}))
+    count = find[0]['count']
+    db.gwanho.update_one({'count': count}, {'$set':{'count': count + 1}})
+    return jsonify({'stats': 'ok'})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
